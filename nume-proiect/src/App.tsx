79238ef15d296f2cee80704488a88e7e90133360
bogdan-ysl <168082641+bogdan-ySL1@users.noreply.github.com>
2024-05-02 11:57:46 +0300

    Laboratorul2
    infromatie student pe site

---
+++ nume-proiect/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Bun venit, [Bogdan] [Costas]!</h1>
+      <p>Informații despre grup:</p>
+      <ul>
+        <li>Numele grupului: [CR-221]</li>
+        <li>Anul de înființare: [2023]</li>
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
